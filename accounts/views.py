a761b7b0400e31291947033e96de5bbb8788d150
Jose <jose@oldcutler.club>
2023-06-06 19:18:35 -0400

    Feature 10 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login as auth_login, logout
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignUpForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -29,3 +31,32 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("login")
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password_confirmation = form.cleaned_data["password_confirmation"]
+
+            if password == password_confirmation:
+                if User.objects.filter(username=username).exists():
+                    raise IntegrityError("This username is already taken")
+                else:
+                    user = User.objects.create_user(
+                        username=username,
+                        password=password,
+                    )
+                    auth_login(request, user)
+                    return redirect("list_projects")
+            else:
+                form.add_error("password", "the passwords do not match")
+    else:
+        form = SignUpForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
