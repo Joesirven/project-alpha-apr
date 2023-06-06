14c19710dc2ecd971202f0a19f7ed766368922c2
Jose <jose@oldcutler.club>
2023-06-06 18:19:51 -0400

    Feature 5 complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Project
+
 
 # Create your views here.
+
+
+def list_projects(request):
+    projects = Project.objects.all()
+    context = {
+        "projects": projects,
+    }
+    return render(request, "projects/list.html", context)
+
+
+# def detail_project(request, id):
+#     recipe = get_object_or_404(Project, id=id)
+#     context = {
+#         "recipe_object": recipe,
+#     }
+#     return render(request, "projects/detail.html", context)
+
+
+# def create_project(request):
+#     recipe =
